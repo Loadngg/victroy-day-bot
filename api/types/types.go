9f2909883ea9c17fc7c87b4e29b8e1d977892d64
Ярослав Аверин <averinj@mail.ru>
2024-10-08 23:10:21 +0300

    > minor update api type + added swagger schema.yaml

--- api/types/types.go
+++ api/types/types.go
@@ -1,20 +1,22 @@
 package types
 
+import "time"
+
 type Path struct {
 	Id     int    `json:"id"`
 	Name   string `json:"name"`
-	Region int    `json:"region,omitempty"`
+	Region *int   `json:"region,omitempty"`
 	Places []int  `json:"places"`
 }
 
 type Place struct {
-	Id             int    `json:"id"`
-	Name           string `json:"name"`
-	Coordinates    string `json:"coordinates"`
-	TextRiddle     string `json:"text_riddle,omitempty"`
-	VideoRiddleUrl string `json:"video_riddle_url"`
-	VideoStoryUrl  string `json:"video_story_url"`
-	TaskFile       string `json:"task_file"`
+	Id             int     `json:"id"`
+	Name           string  `json:"name"`
+	Coordinates    string  `json:"coordinates"`
+	TextRiddle     *string `json:"text_riddle,omitempty"`
+	VideoRiddleUrl string  `json:"video_riddle_url"`
+	VideoStoryUrl  string  `json:"video_story_url"`
+	TaskFile       string  `json:"task_file"`
 }
 
 type Region struct {
@@ -31,11 +33,21 @@ type Team struct {
 }
 
 type TeamPlaceAnswer struct {
-	Id            int    `json:"id"`
-	StartDatetime string `json:"start_datetime,omitempty"`
-	EndDatetime   string `json:"end_datetime,omitempty"`
-	Photo         string `json:"photo,omitempty"`
-	TaskAnswer    string `json:"task_answer,omitempty"`
-	Team          int    `json:"team"`
-	Place         int    `json:"place"`
+	Id            int       `json:"id"`
+	StartDatetime time.Time `json:"start_datetime,omitempty"`
+	EndDatetime   time.Time `json:"end_datetime,omitempty"`
+	Photo         string    `json:"photo,omitempty"`
+	TaskAnswer    string    `json:"task_answer,omitempty"`
+	Team          int       `json:"team"`
+	Place         int       `json:"place"`
+}
+
+type PatchedTeamPlaceAnswer struct {
+	ID            int       `json:"id,omitempty"`
+	StartDatetime time.Time `json:"start_datetime,omitempty"`
+	EndDatetime   time.Time `json:"end_datetime,omitempty"`
+	Photo         string    `json:"photo,omitempty"`
+	TaskAnswer    string    `json:"task_answer,omitempty"`
+	Team          int       `json:"team,omitempty"`
+	Place         int       `json:"place,omitempty"`
 }
